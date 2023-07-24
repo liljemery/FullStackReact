1677738a9a4da75250304a9512b19b80b9162390
Jemery Inoa <jeremyinoa67@gmail.com>
2023-07-23 23:36:35 -0400

    fixed not found page image sizing

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const NotFound = () => {
   return (
     <>
-    <div className='container d-block text-center my-5'>
+    <div className='container text-center my-5'>
       <h1>Your page couldnt be found!</h1> 
       <em className='text-center'>We're working on it, give us just a few moments...</em>
-      <img src="./robot.jpg" alt=""  />
+      <img src="./robot.jpg" alt="" id='notFoundPhoto' />
     </div>
 
     </>
