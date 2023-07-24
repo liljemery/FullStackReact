677281f3938453a80ba439461a76b76d7ae5b82a
Jemery Inoa <jeremyinoa67@gmail.com>
2023-07-24 13:53:34 -0400

    connected front/back end with axios

--- src/Components/ArticlePage.jsx
+++ src/Components/ArticlePage.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import  axios  from 'axios'
 import articles from '../article-content';
 import NotFound from './NotFound';
 
 const ArticlePage = () => {
+  const [ articleInfo, setArticleInfo ] = useState({upvotes: 0, comments: []});
   const { articleId } = useParams();
+
+  useEffect(() => {
+    const loadArticleInfo = async () =>{
+      const response = await axios.get(`http://localhost:8000/api/articles/${articleId}`)
+      const newArticleInfo = response.data;
+      setArticleInfo({newArticleInfo})
+    }
+    loadArticleInfo();
+  },[]);
+
   const article = articles.find( article => article.name === articleId);
+
   if(!article){return <NotFound/>}
 
   return (
     <>
     <div className='container'>
       <h1>{article.title}</h1>
-      <em>{article.name}</em>
-      <div>
+      <em>This article has  {articleInfo.upvotes} upvote(s)</em>
+      <div className='mt-3'>
         {article.content.map((paragraph, i) =>(
           <p key={i}>{paragraph}</p>
         ))}
