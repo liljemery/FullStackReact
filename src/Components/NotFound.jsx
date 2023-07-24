9c9da5c09d72ea1ca6c790ab931e12ef4d7b954e
Jemery Inoa <jeremyinoa67@gmail.com>
2023-07-24 09:19:48 -0400

    fixed not found img sizing

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -5,7 +5,6 @@ const NotFound = () => {
     <>
     <div className='container text-center my-5'>
       <h1>Your page couldnt be found!</h1> 
-      <em className='text-center'>We're working on it, give us just a few moments...</em>
       <img src="./robot.jpg" alt="" id='notFoundPhoto' />
     </div>
 
