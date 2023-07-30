e1727a911583a8819da338678c61aff120ce41d5
Jemery Inoa <jeremyinoa67@gmail.com>
2023-07-30 00:08:25 -0400

    fix: removed firebase key

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCduZe5IRI-Z7ExK8CMgk_79EGs8_DqO8Y",
-  authDomain: "my-react-blog-d8cb8.firebaseapp.com",
-  projectId: "my-react-blog-d8cb8",
-  storageBucket: "my-react-blog-d8cb8.appspot.com",
-  messagingSenderId: "34387217235",
-  appId: "1:34387217235:web:14193f675000d17e536fa6"
-};
+import firebaseConfig from './firebaseConfig.js';
 
 const app = initializeApp(firebaseConfig);
 
